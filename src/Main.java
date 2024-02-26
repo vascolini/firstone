8d2d40939c3e20f5d2638cdab86fbe605664bac7
vasco <vascolini.com@outlook.com>
2024-02-26 01:43:42 +0000

    this is the main version

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Vasco alves!");
+
+        System.out.println("Mechanical engineering ");
+
+        System.out.println("Pardubice");
+    }
+}
